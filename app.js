6cd87af743324bb5426dbeadfb2c41a49ddad903
DESKTOP-12JL2VT\pc <godkor200@gmail.com>
2021-10-27 16:07:14 +0900

    디비 연결 완료

--- app.js
+++ app.js
@@ -6,43 +6,39 @@ const port = 3000;
 
 require("dotenv").config();
 const dbConfig = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    connectString: process.env.DB_connectString,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  connectString: process.env.DB_connectString,
 };
 
 app.get("/", function (request, response, next) {
-    console.log("!11111");
-    oracledb.getConnection(
-        { user: dbConfig.user, password: dbConfig.password, connectString: dbConfig.connectString },
-        function (err, connection) {
-            if (err) {
-                console.error(err.message);
-                return;
-            }
-            let query = "select * " + " from MENBERS";
-            connection.execute(query, [], function (err, result) {
-                if (err) {
-                    console.error(err.message);
-                    doRelease(connection);
-                    return;
-                }
-                res.send(result);
-                doRelease(connection, result.rows);
-            });
-        }
-    );
-    function doRelease(connection, rowList) {
-        connection.release(function (err) {
-            if (err) {
-                console.error(err.message);
-            }
-            console.log("list size: " + rowList.length);
-            response.send(rowList);
-        });
+  oracledb.getConnection({ user: dbConfig.user, password: dbConfig.password, connectString: dbConfig.connectString }, function (err, connection) {
+    if (err) {
+      console.error(err.message);
+      return;
     }
+    let query = "select * " + " from MEMBERS";
+    connection.execute(query, [], function (err, result) {
+      if (err) {
+        console.error(err.message);
+        doRelease(connection);
+        return;
+      }
+      response.send(result);
+      doRelease(connection, result.rows);
+    });
+  });
+  function doRelease(connection, rowList) {
+    connection.release(function (err) {
+      if (err) {
+        console.error(err.message);
+      }
+      console.log("list size: " + rowList.length);
+      response.send(rowList);
+    });
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
